cb72564ee9f15df2c1d33cb1f2f8432bd9955393
charuzain <charuabhizain@gmail.com>
2022-01-14 17:40:47 -0500

    Add Backend Setup

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+const db = require('./db');
+var express = require('express');
+var path = require('path');
+
+const dbHelpers = require('./helpers/dbHelpers')(db);
+
+
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/api/users', usersRouter(dbHelpers));
+
+module.exports = app;
