26543a36bf6eb07f4079c43e5791da7e69444f7d
charuzain <charuabhizain@gmail.com>
2022-01-16 12:31:03 -0500

    Add Product routes

---
+++ backend/routes/products.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+module.exports = ({getAllProducts, getSingleProduct})=>{
+router.get('/',(req,res)=>{
+  getAllProducts()
+  .then((products)=>res.json(products))
+    .catch((err) => res.json({
+      error: err.message
+    }));
+    
+})
+
+router.get('/:id',(req,res)=>{
+  const id = req.params.id
+  getSingleProduct(id)
+    .then((product)=>{res.json(product)})
+    .catch((err) => res.json({
+      error: err.message
+    }));
+})
+
+  return router;
+}
