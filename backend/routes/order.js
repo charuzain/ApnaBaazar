bb76dad76af61ce6440631c98992459bc011e73a
Gouthami Kolla <89610131+gouthamikolla@users.noreply.github.com>
2022-01-19 14:24:28 -0500

    Add order route

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,29 +1,37 @@
+/* eslint-disable camelcase */
 const express = require("express");
 const router = express.Router();
-module.exports = ({}) => {
-
+module.exports = ({ getOrderById, addCartToDB }) => {
   //get order by id
-  router.get('/', (req, res) => {
-    // const userId = req.session
-    const userId= req.session.us
-    getUserOrders(UserId)
-      .then((products) => res.json(products))
-      .catch((err) => res.json({
-        error: err.message
-      })); 
+  router.post("/:orderId", (req, res) => {
+    const { user_id, cart } = req.body;
 
-  })
+    getOrderById(user_id, cart)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+      })
+      .then((order) => res.json(order))
+      .catch((err) => res.json({ error: err.message }));
+  });
 
   // create order
-
-  router.post('',(req,res)=>{
-
-
-  })
-
-
-
- 
+  //post:/api/orders
+  // submits an order;
+  router.post("/", (req, res) => {
+    const { user_id, cart } = req.body; // here we are passing user-id and cart
+    addCartToDB(user_id, cart)
+      .then((result) => {
+        console.log(result);
+        return res.json(result.order_id);
+      })
+      .catch((err) =>
+        res.json({
+          error: err.message,
+        })
+      );
+  });
 
   return router;
-}
+};
