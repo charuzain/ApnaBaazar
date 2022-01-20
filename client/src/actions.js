1b8a98340daf72871b21b1165a84acf91407996a
charuzain <charuabhizain@gmail.com>
2022-01-20 02:59:09 -0500

    Add action for useReducer hook

--- client/src/actions.js
+++ client/src/actions.js
@@ -3,4 +3,5 @@ export const GET_PRODUCTS_END = 'GET_PRODUCT_END'  // get products without any e
 export const GET_PRODUCTS_ERROR = 'GET_PRODUCTS_ERROR' //error while loading products
 export const GET_SINGLE_PRODUCT_BEGIN = 'GET_SINGLE_PRODUCT_BEGIN';
 export const GET_SINGLE_PRODUCT_END = 'GET_SINGLE_PRODUCT_END';
+export const GET_SINGLE_PRODUCT_ERROR = 'GET_SINGLE_PRODUCT_ERROR';
 
