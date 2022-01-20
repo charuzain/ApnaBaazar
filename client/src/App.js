102b8cbb8b6be8bf0b6a536d37a8d92c60556b6d
charuzain <charuabhizain@gmail.com>
2022-01-20 11:44:27 -0500

    Add routes

--- client/src/App.js
+++ client/src/App.js
@@ -9,22 +9,11 @@ import SingleProductPage from "./pages/SingleProductPage";
 // import { Fresh } from "./components/Fresh";
 
 const App = () => {
-  // const [fresh, setFresh] = useState([])
-
-  // useEffect(() => {
-  //   axios.get("http://localhost:3001/api/products").then((res) => {
-  //     setFresh(res.data);
-  //     console.log(res.data)
-
-  //   });
-  // }, []);
 
   return (
   <Routes>
-      {/* <Fresh fresh={fresh}/> */}
       <Route path='/' element={<FeaturedProducts />}></Route>
       <Route path='products/:id' element={<SingleProductPage />}></Route>
-
     </Routes>
   );
 };
