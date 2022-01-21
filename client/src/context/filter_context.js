cce93e377f03fd9c843359e256b106add36bfb98
charuzain <charuabhizain@gmail.com>
2022-01-21 02:16:26 -0500

    setview and gridview action dispatch

--- client/src/context/filter_context.js
+++ client/src/context/filter_context.js
@@ -30,8 +30,15 @@ export const FilterProvider = ({ children }) => {
 
   },[products])
   // console.log(state)
+  const setGridView=()=>{
+    dispatch({type:SET_GRIDVIEW})
+  }
+
+  const setListView = () => {
+    dispatch({ type: SET_LISTVIEW })
+  }
   return (
-    <FilterContext.Provider value={{...state}}>
+    <FilterContext.Provider value={{...state,setGridView,setListView}}>
       {children}
     </FilterContext.Provider>
   )
