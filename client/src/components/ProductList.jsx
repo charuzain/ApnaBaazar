1d7c4a985656c8626f613d9521c9882543344658
charuzain <charuabhizain@gmail.com>
2022-01-21 01:30:15 -0500

    Render ListView

--- client/src/components/ProductList.jsx
+++ client/src/components/ProductList.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import { FaHtml5 } from 'react-icons/fa';
 import  {useFilterContext}  from '../context/filter_context'
 import GridView from './Gridview';
+import ListView from './ListView';
 
 
 export default function ProductList() {
-  const {filtered_products:products} = useFilterContext();
-  return (
-    <div>
-      <GridView products={products}>Product List</GridView>
-    </div>
-  )
+  const {filtered_products:products,grid_view} = useFilterContext();
+  if (products.length < 1) {
+    return (
+      <h5 style={{ textTransform: 'none' }}>
+        No products matched your search...
+      </h5>
+    )
+  }
+  if (grid_view === false) {
+    return <ListView products={products} />
+  }
+  return <GridView products={products}>product list</GridView>
 }
+
