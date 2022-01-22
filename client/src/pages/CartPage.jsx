9146439c0a0b764f988e18778ce4562ad71504e8
charuzain <charuabhizain@gmail.com>
2022-01-22 00:37:58 -0500

    Add cartPAge

--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
+import { useCartContext } from '../context/cart_context'
+import { Link } from 'react-router-dom'
+import { CartContent, PageHero } from '../components'
 
-export default function CartPage() {
-  return (
-    <div>
-      
-    </div>
-  )
+const CartPage = () => {
+  return <h4>cart page</h4>
 }
+
+const Wrapper = styled.main`
+  .empty {
+    text-align: center;
+    h2 {
+      margin-bottom: 1rem;
+      text-transform: none;
+    }
+  }
+`
+
+export default CartPage
