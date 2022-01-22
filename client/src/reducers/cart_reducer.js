111b4a447e77f019541346b52e8f8d7d421f21e2
charuzain <charuabhizain@gmail.com>
2022-01-22 14:36:16 -0500

    AddToCart reducer : existing item

--- client/src/reducers/cart_reducer.js
+++ client/src/reducers/cart_reducer.js
@@ -13,11 +13,12 @@ const cart_reducer = (state, action) => {
     // check if the item is already added in the cart
     const tempItem = state.cart.find((i) => i.id === id)
 
-    // if item exist in the cart 
+    // if item exist in the cart , iterate and check where item is update the amount 
     if (tempItem) {
       const tempCart = state.cart.map((cartItem) => {
-        if (cartItem.id === id ) {
+        if (cartItem.id === id) {
           let newAmount = cartItem.amount + amount
+          // check the inventory
           if (newAmount > cartItem.max) {
             newAmount = cartItem.max
           }
@@ -28,15 +29,16 @@ const cart_reducer = (state, action) => {
       })
 
       return { ...state, cart: tempCart }
-    } else {
-      const newItem = {
-        id: id,
+    } else {    // if item doesnot exist in cart , create and add a new item to cart
+      const newItem = {         // create new item
+        id: id,                 //product id
         name: product.name,
         amount,
         image: product.image,
         price: product.price,
         max: product.quantity,
       }
+      console.log(newItem)
       return { ...state, cart: [...state.cart, newItem] }
     }
   }
