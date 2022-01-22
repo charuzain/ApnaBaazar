893891227e3dd6cc61cc978fad74e0e8f554ba42
charuzain <charuabhizain@gmail.com>
2022-01-22 18:01:02 -0500

    complete CartItem component

--- client/src/components/Cart/CartItem.jsx
+++ client/src/components/Cart/CartItem.jsx
@@ -2,10 +2,35 @@ import React from 'react'
 import styled from 'styled-components'
 // import { formatPrice } from '../utils/helpers'
 // import AmountButtons from './AmountButtons'
-// import { FaTrash } from 'react-icons/fa'
-// import { useCartContext } from '../context/cart_context'
-const CartItem = () => {
-  return <h4>cart item</h4>
+import { FaTrash } from 'react-icons/fa'
+import { useCartContext } from '../../context/cart_context'
+import AmountButtons from '../AmountButtons'
+const CartItem = ({id,name,image,price,amount}) => {
+  const {removeItem ,toggleAmount} = useCartContext();
+  const increase =()=>{}
+  const decrease=()=>{}
+
+  return (
+    <Wrapper>
+      <div className="title">
+        <img src={image} alt={name} />
+        <div>
+          <h5 className="name">
+            {name}
+          </h5>
+          <h5 className="price-small">${price}.00</h5>
+        </div>
+
+      </div>
+      <h5 className="price">${price}.00</h5>
+      <AmountButtons amount={amount} increase={increase} decrease={decrease}/>
+      <h5 className="subtotal">${price*amount}.00</h5>
+      <button type="button" className="remove-btn" onClick={()=>removeItem(id)}>
+
+        <FaTrash />
+      </button>
+    </Wrapper>
+  )
 }
 
 
