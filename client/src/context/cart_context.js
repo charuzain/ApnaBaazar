67705282e65eac400531ea416303793f1ef2f986
charuzain <charuabhizain@gmail.com>
2022-01-22 14:35:30 -0500

    Add localStorage

--- client/src/context/cart_context.js
+++ client/src/context/cart_context.js
@@ -8,8 +8,21 @@ import {
   COUNT_CART_TOTALS,
 } from '../actions'
 
+// check if an item by the name cart exist in local storage 
+const getLocalStorage = () => {
+  
+  let cart = localStorage.getItem('cart');
+  //console.log(cart);
+  if (cart) {
+    return JSON.parse(localStorage.getItem('cart'))
+  }
+  else {
+    return []
+  }
+}
+
 const initialState = {
-  cart: [],
+  cart: getLocalStorage(),
   total_items: 0,
   total_amount: 0
 }
@@ -17,14 +30,33 @@ const initialState = {
 const CartContext = React.createContext()
 
 export const CartProvider = ({ children }) => {
+
   const [state, dispatch] = useReducer(reducer, initialState)
 
-  // Add to cart
+  // Add item to cart , invoke it at singleProduct Page
   const addToCart = (id, amount, product) => {
     dispatch({ type: ADD_TO_CART, payload: { id, amount, product } })
   }
+
+  // function to remove item from cart
+  const removeItem = (id) => { }
+
+  const toggleAmount = (id, value) => { }
+
+  //function to clear cart
+  const clearCart = () => { }
+
+  // we are not persisiting the cart data
+  //everytime there is change in cart 
+  //invoke useEffect and save current cart in local storage
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(state.cart))
+  }, [state.cart]) // invoke everytime there is a change in cart
+
   return (
-    <CartContext.Provider value={{ ...state, addToCart }}>{children}</CartContext.Provider>
+    <CartContext.Provider value={{ ...state, addToCart, removeItem, toggleAmount, clearCart }}>
+      {children}
+    </CartContext.Provider>
   )
 }
 // make sure use
