f71444a3c1ac0eaa4243049eff9ad67225f02d95
charuzain <charuabhizain@gmail.com>
2022-01-22 21:06:38 -0500

    complete cartContent

--- client/src/components/Cart/CartContent.jsx
+++ client/src/components/Cart/CartContent.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom'
 import CartColumns from './CartColumns'
 import CartItem from './CartItem.jsx'
 import CartTotals from './CartTotals'
+import CartButtons from './CartButtons'
 
 const CartContent = () => {
   const { cart, clearCart } = useCartContext()
@@ -20,6 +21,7 @@ const CartContent = () => {
       <button type='button' className='link-btn clear-btn' onClick={clearCart}>Clear Shopping Cart</button>
       </div>
       <CartTotals />
+      <CartButtons/>
     </Wrapper>
   )
 }
