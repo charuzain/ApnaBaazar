34c09fd1e517b6774930f2ffd39a2cc67f3b3ee4
RAJINIA <rajini1608@gmail.com>
2022-01-23 10:29:29 -0600

    Add Deleted Login Functionality

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -38,7 +38,7 @@ const Login = () => {
         <div class="row mb-3">
           <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
           <div class="col-sm-10">
-            <input type="email" class="form-control" id="inputEmail3" 
+          <input type="email" class="form-control" id="inputEmail3" 
             onChange={(e) => {
               setEmail(e.target.value);
             }}
@@ -48,13 +48,14 @@ const Login = () => {
         <div class="row mb-3">
           <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
           <div class="col-sm-10">
-            <input type="password" class="form-control" id="inputPassword3" 
+          <input type="password" class="form-control" id="inputPassword3" 
             onChange={(e) => {
               setPassword(e.target.value);
             }}
             placeholder="Password" />
           </div>
         </div>
+
 
         <button type="submit" class="btn btn-primary" onClick={login}>Sign in</button>
       </form>
