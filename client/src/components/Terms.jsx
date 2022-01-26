42d13fdf706d9e9314b3c6933426621c11f88b90
RAJINIA <rajini1608@gmail.com>
2022-01-25 21:40:02 -0600

    Add Terms & Policy

---
+++ client/src/components/Terms.jsx
@@ -0,0 +1,174 @@
+import React from 'react';
+import styled from 'styled-components'
+import Header from './Header';
+import Footer from './Footer';
+
+export default function termsAndConditions() {
+  return (
+    <Wrapper>
+      <div className='body-container'>
+        <Header />
+        <div class="about-section">
+          <h1>Terms & Conditions </h1>
+          <p>Welcome to Apna Basket. The following terms, as updated from time to time (collectively, the "Terms") govern your access to and use of all or part of www.apnabazaar.ca, and any other authorized websites, mobile applications, in-warehouse kiosks or other online services, tools or programs where these Terms appear (collectively, the "Services"). The Services are provided by VCAP Trading Corp. or one of its affiliated or subsidiary companies (collectively, or individually, as the case may be, "VCAP"). By visiting or using the Services, you expressly agree to these Terms. We may change or modify these Terms from time to time and will post the updated Terms with a "Last Updated" effective date of the revision. Any changes or modifications will be effective immediately upon posting of the changes or modifications, and you waive any right you may have to receive specific notice of such changes or modifications, and to the extent permitted by law you waive any right you may have to receive specific notice of such changes or modifications. Your continued access to, use of and/or participation in the Services after these Terms have been changed or modified (see Last Updated date above) signifies your acceptance of any updated Terms. As a result, you should frequently review these Terms to understand the terms and conditions that apply to your access to, use of and/or participation in the Services. If you do not agree to the changed or modified Terms, do not use the Services.
+
+            By visiting our site and/ or purchasing something from us, you engage in our “Service” and agree to be bound by the following terms and conditions (“Terms of Service”, “Terms”), including those additional terms and conditions and policies referenced herein and/or available by hyperlink. These Terms of Service apply to all users of the site, including without limitation users who are browsers, vendors, customers, merchants, and/ or contributors of content.
+
+            Please read these Terms of Service carefully before accessing or using our website. By accessing or using any part of the site, you agree to be bound by these Terms of Service. If you do not agree to all the terms and conditions of this agreement, then you may not access the website or use any services. If these Terms of Service are considered an offer, acceptance is expressly limited to these Terms of Service.
+
+            Any new features or tools which are added to the current store shall also be subject to the Terms of Service. You can review the most current version of the Terms of Service at any time on this page. We reserve the right to update, change or replace any part of these Terms of Service by posting updates and/or changes to our website. It is your responsibility to check this page periodically for changes. Your continued use of or access to the website following the posting of any changes constitutes acceptance of those changes.</p>
+
+          <h4>ONLINE STORE TERMS</h4>
+          <p>By agreeing to these Terms of Service, you represent that you are at least the age of majority in your state or province of residence, or that you are the age of majority in your state or province of residence and you have given us your consent to allow any of your minor dependents to use this site.
+
+            You may not use our products for any illegal or unauthorized purpose nor may you, in the use of the Service, violate any laws in your jurisdiction (including but not limited to copyright laws).
+
+            You must not transmit any worms or viruses or any code of a destructive nature.
+
+            A breach or violation of any of the Terms will result in an immediate termination of your Services.</p>
+
+          <h4>ONLINE STORE TERMS</h4>
+          <p>We reserve the right to refuse service to anyone for any reason at any time.
+
+            You understand that your content (not including credit card information), may be transferred unencrypted and involve (a) transmissions over various networks; and (b) changes to conform and adapt to technical requirements of connecting networks or devices. Credit card information is always encrypted during transfer over networks.
+
+            You agree not to reproduce, duplicate, copy, sell, resell or exploit any portion of the Service, use of the Service, or access to the Service or any contact on the website through which the service is provided, without express written permission by us.
+
+            The headings used in this agreement are included for convenience only and will not limit or otherwise affect these Terms.</p>
+
+          <h4>ACCURACY, COMPLETENESS AND TIMELINESS OF INFORMATION</h4>
+          <p>We are not responsible if information made available on this site is not accurate, complete or current. The material on this site is provided for general information only and should not be relied upon or used as the sole basis for making decisions without consulting primary, more accurate, more complete or more timely sources of information. Any reliance on the material on this site is at your own risk.
+
+            This site may contain certain historical information. Historical information, necessarily, is not current and is provided for your reference only. We reserve the right to modify the contents of this site at any time, but we have no obligation to update any information on our site. You agree that it is your responsibility to monitor changes to our site.</p>
+
+          <h4>MODIFICATIONS TO THE SERVICE AND PRICES</h4>
+          <p>Prices for our products are subject to change without notice.
+
+            We reserve the right at any time to modify or discontinue the Service (or any part or content thereof) without notice at any time.
+
+            We shall not be liable to you or to any third-party for any modification, price change, suspension or discontinuance of the Service.</p>
+
+          <h4>PRODUCTS OR SERVICES (if applicable)</h4>
+          <p>Certain products or services may be available exclusively online through the website. These products or services may have limited quantities and are subject to return or exchange only according to our Return Policy.
+
+            We have made every effort to display as accurately as possible the colors and images of our products that appear at the store. We cannot guarantee that your computer monitor's display of any color will be accurate.
+
+            We reserve the right, but are not obligated, to limit the sales of our products or Services to any person, geographic region or jurisdiction. We may exercise this right on a case-by-case basis. We reserve the right to limit the quantities of any products or services that we offer. All descriptions of products or product pricing are subject to change at anytime without notice, at the sole discretion of us. We reserve the right to discontinue any product at any time. Any offer for any product or service made on this site is void where prohibited.
+
+            We do not warrant that the quality of any products, services, information, or other material purchased or obtained by you will meet your expectations, or that any errors in the Service will be corrected.</p>
+
+          <h4>ACCURACY OF BILLING AND ACCOUNT INFORMATION</h4>
+          <p>We reserve the right to refuse any order you place with us. We may, in our sole discretion, limit or cancel quantities purchased per person, per household or per order. These restrictions may include orders placed by or under the same customer account, the same credit card, and/or orders that use the same billing and/or shipping address. In the event that we make a change to or cancel an order, we may attempt to notify you by contacting the e-mail and/or billing address/phone number provided at the time the order was made. We reserve the right to limit or prohibit orders that, in our sole judgment, appear to be placed by dealers, resellers or distributors.
+
+            You agree to provide current, complete and accurate purchase and account information for all purchases made at our store. You agree to promptly update your account and other information, including your email address and credit card numbers and expiration dates, so that we can complete your transactions and contact you as needed.
+
+            For more detail, please review our Returns Policy.</p>
+
+          <h4>OPTIONAL TOOLS</h4>
+          <p>We may provide you with access to third-party tools over which we neither monitor nor have any control nor input.
+
+            You acknowledge and agree that we provide access to such tools ”as is” and “as available” without any warranties, representations or conditions of any kind and without any endorsement. We shall have no liability whatsoever arising from or relating to your use of optional third-party tools.
+
+            Any use by you of optional tools offered through the site is entirely at your own risk and discretion and you should ensure that you are familiar with and approve of the terms on which tools are provided by the relevant third-party provider(s).
+
+            We may also, in the future, offer new services and/or features through the website (including, the release of new tools and resources). Such new features and/or services shall also be subject to these Terms of Service.</p>
+
+          <h4>THIRD-PARTY LINKS</h4>
+          <p>Certain content, products and services available via our Service may include materials from third-parties.
+
+            Third-party links on this site may direct you to third-party websites that are not affiliated with us. We are not responsible for examining or evaluating the content or accuracy and we do not warrant and will not have any liability or responsibility for any third-party materials or websites, or for any other materials, products, or services of third-parties.
+
+            We are not liable for any harm or damages related to the purchase or use of goods, services, resources, content, or any other transactions made in connection with any third-party websites. Please review carefully the third-party's policies and practices and make sure you understand them before you engage in any transaction. Complaints, claims, concerns, or questions regarding third-party products should be directed to the third-party.</p>
+
+          <h4>USER COMMENTS, FEEDBACK AND OTHER SUBMISSIONS</h4>
+          <p>If, at our request, you send certain specific submissions (for example contest entries) or without a request from us you send creative ideas, suggestions, proposals, plans, or other materials, whether online, by email, by postal mail, or otherwise (collectively, 'comments'), you agree that we may, at any time, without restriction, edit, copy, publish, distribute, translate and otherwise use in any medium any comments that you forward to us. We are and shall be under no obligation (1) to maintain any comments in confidence; (2) to pay compensation for any comments; or (3) to respond to any comments.
+
+            We may, but have no obligation to, monitor, edit or remove content that we determine in our sole discretion are unlawful, offensive, threatening, libelous, defamatory, pornographic, obscene or otherwise objectionable or violates any party’s intellectual property or these Terms of Service.
+
+            You agree that your comments will not violate any right of any third-party, including copyright, trademark, privacy, personality or other personal or proprietary right. You further agree that your comments will not contain libelous or otherwise unlawful, abusive or obscene material, or contain any computer virus or other malware that could in any way affect the operation of the Service or any related website. You may not use a false e-mail address, pretend to be someone other than yourself, or otherwise mislead us or third-parties as to the origin of any comments. You are solely responsible for any comments you make and their accuracy. We take no responsibility and assume no liability for any comments posted by you or any third-party.</p>
+
+          <h4>PERSONAL INFORMATION</h4>
+          <p>Your submission of personal information through the store is governed by our Privacy Policy. To view our Privacy Policy.</p>
+
+
+
+        </div>
+      </div>
+
+
+      <Footer />
+    </Wrapper>
+  );
+} const Wrapper = styled.section`
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0;
+}
+
+html {
+  box-sizing: border-box;
+}
+
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+
+.column {
+  float: left;
+  width: 33.3%;
+  margin-bottom: 16px;
+  padding: 0 8px;
+}
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  margin: 8px;
+}
+
+.about-section {
+  margin-top: 20px;
+  padding: 50px;
+  text-align: left;
+  background-color: orange;
+  color: white;
+}
+
+.container {
+  padding: 0 16px;
+}
+
+.container::after, .row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+.title {
+  color: grey;
+}
+
+.button {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 8px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+}
+
+.button:hover {
+  background-color: #555;
+}
+
+@media screen and (max-width: 650px) {
+  .column {
+    width: 100%;
+    display: block;
+  }
+}
+.about_team {
+  text-align: left; 
+}`
